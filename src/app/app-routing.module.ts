c96b57b37a8e76982946f9ccfe0e232f07325172
Rishabh Sharma <rish6501@gmail.com>
2024-02-13 00:15:41 +0530

    Added Checkout Components on Cart-details Page and routing
    Added Checkout Components on Cart-details Page and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 
 
 // paths priority goes from top to bottom , so generic is defined at the bottom
 const routes: Routes = [
+  {path: 'checkout',component: CheckoutComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'search/:keyword', component: ProductListComponent},

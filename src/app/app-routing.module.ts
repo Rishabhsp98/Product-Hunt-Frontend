65240121e2ee2a8984d1bdeab460598f800ef923
Rishabh Sharma <rish6501@gmail.com>
2024-02-20 19:57:36 +0530

    Okta Login-logout security implementation
    Okta Login-logout security implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injector, NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
+import { LoginComponent } from './components/login/login.component';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
+import { MembersPageComponent } from './components/members-page/members-page.component';
+import { OktaAuth } from '@okta/okta-auth-js';
 
+
+function sendToLoginPage(oktaAuth: OktaAuth,injector : Injector){
+  //use Injector to access any service available within the application
+  const router = injector.get(Router);
+
+  //redirect user to custom login page
+  router.navigate(['/login']);
+}
 
 // paths priority goes from top to bottom , so generic is defined at the bottom
 const routes: Routes = [
+
+  {path: 'members', component: MembersPageComponent, canActivate : [OktaAuthGuard],
+              data: {onAuthRequired : sendToLoginPage}},
+  {path: 'login/callback', component: OktaCallbackComponent},
+  {path: 'login', component: LoginComponent},
   {path: 'checkout',component: CheckoutComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oaf8cpvjihC222ap5d7',
+        issuer: 'https://dev-64273663.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
